fe383dcb2ad85ce95b8afc3617b75e9b0523790c
djahasiel16 <djahasiel@gmail.com>
2025-07-30 17:21:52 +0800

    Working api both POST and GET request

--- api/apps/iar/serializers.py
+++ api/apps/iar/serializers.py
@@ -1,12 +1,21 @@
 from .models import *
 from rest_framework import serializers
 from apps.core.serializers import OfficeSerializer, SupplierSerializer, EmployeeSerializer
+from apps.core.models import Office, Supplier, Employee
 
 class InspectionAcceptanceReportSerializer(serializers.ModelSerializer):
-    office = OfficeSerializer()
-    supplier = SupplierSerializer()
-    receivedBy = EmployeeSerializer()
-    submittedBy = EmployeeSerializer()
+    supplier = serializers.PrimaryKeyRelatedField(queryset=Supplier.objects.all())
+    supplier_details = SupplierSerializer(source='supplier', read_only=True)
+
+    receivedBy = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all())
+    receivedBy_details = EmployeeSerializer(source='receivedBy', read_only=True)
+
+    submittedBy = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all())
+    submittedBy_details = EmployeeSerializer(source='submittedBy', read_only=True)
+
+    office = serializers.PrimaryKeyRelatedField(queryset=Office.objects.all())
+    office_details = OfficeSerializer(source='office',read_only=True)
+
     
     class Meta:
         model = InspectionAcceptanceReport
@@ -25,8 +34,12 @@ class InspectionAcceptanceReportSerializer(serializers.ModelSerializer):
             'submittedBy',
             'office',
             'remarks',
+            'supplier_details',
+            'submittedBy_details',
+            'receivedBy_details',
+            'office_details',
         ]
-        read_only_fields = ['createdBy', 'createdAt', 'updatedAt']
+        # read_only_fields = ['createdBy', 'createdAt', 'updatedAt']
         
 class ParticularSerializer(serializers.ModelSerializer):
     class Meta:
